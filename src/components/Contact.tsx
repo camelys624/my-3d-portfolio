24c2bed8f09de49bd24219541ea1e3327b6efe3b
camel_y <camel_yangz@163.com>
2023-11-29 21:56:35 +0800

    test action

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -74,7 +74,7 @@ const Contact = ({ translateToggle }:any) => {
                         type="submit"
                         className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
                     >
-                        {loading ? 'Sending...' : 'Send'}
+                        {loading ? 'Sending...' : 'Sendaa'}
                     </button>
                 </form>
             </motion.div>
